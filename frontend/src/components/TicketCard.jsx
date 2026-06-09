21ddbc533b79b9532edcdef4681e89b8d36c3f0f
Durgam <2488214@cognizant.com>
2026-06-09 16:01:26 +0530

    Fixes in ticket summary attempt 4

--- frontend/src/components/TicketCard.jsx
+++ frontend/src/components/TicketCard.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import html2pdf from 'html2pdf.js/dist/html2pdf.min.js';
 import { otpAPI, reviewAPI } from '../utils/api';
 import OtpCancelModal from './OtpCancelModal';
